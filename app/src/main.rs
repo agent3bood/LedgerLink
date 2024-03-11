c32f02841e2e79214d4b4e71b91ebdb91e660f30
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2024-03-11 22:14:16 +0300

    app

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,7 +2,6 @@ use ledger::chain::Chain;
 use network::Network;
 use node::node::Node;
 use std::env;
-use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,10 +10,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let public_key = env::var("KEY_PUB").expect("KEY_PUB must be set");
     let private_key = env::var("KEY_PRIV").expect("KEY_PRIV must be set");
 
-    let chain: Chain = Chain::new();
-    let node = Node::new(public_key, private_key, Arc::new(Mutex::new(chain)))?;
-    let network = Network::new(Arc::new(Mutex::new(node)));
-    let _ = network.run().await;
+    let (network_tx, mut network_rx) = tokio::sync::mpsc::channel(100);
+    let (node_tx, mut node_rx) = tokio::sync::mpsc::channel(100);
+
+    let chain = Chain::new();
+    let node = Node::new(public_key, private_key, chain, node_rx, network_tx)?;
+    let network = Network::new(node.id.clone(), network_rx, node_tx);
+
+    let _ = tokio::join!(node.run(), network.run());
 
     Ok(())
 }
